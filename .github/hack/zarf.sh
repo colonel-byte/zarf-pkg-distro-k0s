f89238da8f6ea705496180b3f6e3ba489aaf1766
Allen Conlon <software@conlon.dev>
2025-08-03 19:20:16 -0400

    feat: add k0sctl binary to package

--- .github/hack/zarf.sh
+++ .github/hack/zarf.sh
@@ -7,6 +7,7 @@ rm -rf   .direnv/bin
 mkdir -p .direnv/bin
 
 export K0S_VERSION=$(yq '.package.create.set.k0s_version' zarf-config.yaml)
+export K0SCTL_VERSION=$(yq '.package.create.set.k0sctl_version' zarf-config.yaml)
 declare -a ARCH=("amd64" "arm64")
 
 for arch in "${ARCH[@]}"; do
@@ -19,6 +20,19 @@ for arch in "${ARCH[@]}"; do
   echo "::debug::sha='$sha'"
 
   export yq_sha=$(printf '.package.create.set.k0s_sha_%s = "%s"' "$arch" "$sha")
+  echo "::debug::yq_sha='$yq_sha'"
+
+  yq -i "$yq_sha" zarf-config.yaml
+
+  echo "::debug::message='downloading k0sctl-$arch for $K0SCTL_VERSION'"
+  curl ${GITHUB_TOKEN:+" -u \":$GITHUB_TOKEN\""} -s -L -o .direnv/bin/k0sctl-$arch https://github.com/k0sproject/k0sctl/releases/download/$K0SCTL_VERSION/k0sctl-linux-$arch
+
+  chmod +x .direnv/bin/k0sctl-$arch
+
+  export sha=$(sha256sum .direnv/bin/k0sctl-$arch | awk '{ print $1 }')
+  echo "::debug::sha='$sha'"
+
+  export yq_sha=$(printf '.package.create.set.k0sctl_sha_%s = "%s"' "$arch" "$sha")
   echo "::debug::yq_sha='$yq_sha'"
 
   yq -i "$yq_sha" zarf-config.yaml
